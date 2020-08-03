c45a4bd5ee4151ece716766885f90e715e411245
annivajo <annivajo@gmail.com>
2020-08-03 15:04:11 +0300

    Add redux: directory, shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,16 +3,20 @@ import {persistReducer} from "redux-persist";
 import storage from 'redux-persist/lib/storage'; //local storage;
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['cart'] //only cart, because user is persist by firebase
+    whitelist: ['cart', 'directory', 'shop'] //only cart, because user is persist by firebase
 };
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
