c8ab5dd5eeef51ac03f727e3caa7f1b51d35899d
annivajo <annivajo@gmail.com>
2020-08-03 12:18:58 +0300

    Add checkout page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
@@ -48,6 +49,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={Homepage}/>
                     <Route path='/shop' component={ShopPage}/>
+                    <Route exact path='/checkout' component={CheckoutPage}/>
                     <Route
                         exact path='/signin'
                         render={() => this.props.currentUser ? (
